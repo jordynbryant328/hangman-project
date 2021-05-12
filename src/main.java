3877b73058f8b5ce4be00d574edfed5fe2f59b40
jordynbryant328 <80836523+jordynbryant328@users.noreply.github.com>
2021-05-11 21:20:31 -0600

    Update main.java

--- src/main.java
+++ src/main.java
@@ -6,41 +6,45 @@ import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-   String word = "open";
+   Scanner input = new Scanner(System.in);
    char [] key = {'o', 'p', 'e', 'n',};
-   String guessChar;
-   int lives = 10;
+   char guessChar;
+   boolean lives;
+   int life = 10;
    
-   while (lives>0) {
+   while (lives=true) {
    System.out.println("Welcome to the hangman game! Enter your first guess:");
-   guessChar = input.next();
-   if (guessChar = key[1]) {
-     System.out.println("" + key[1] + "");
-   } else if (guessChar != key[1]) {
-     lives--;
+   guessChar = input.next().charAt(0);
+   if (guessChar == key[0]) {
+     System.out.println("" + key[0] + "");
+   } else if (guessChar != key[0]) {
+     life--;
      System.out.println("Try again:");
+     guessChar = input.next().charAt(0);
    }
-   if (guessChar = key[2]) {
-     System.out.println("" + key[1] + "" + key[2]);
-   } else if (guessChar != key[2]) {
-     lives--;
+   if (guessChar == key[1]) {
+     System.out.println("" + key[0] + "" + key[1]);
+   } else if (guessChar != key[1]) {
+     life--;
      System.out.println("Try again:");
+     guessChar = input.next().charAt(0);
    }
    
-    if (guessChar = key[3]) {
-     System.out.println("" + key[1] + "" + key[2] + "" + key[3]);
+    if (guessChar == key[2]) {
+     System.out.println("" + key[0] + "" + key[1] + "" + key[2]);
+   } else if (guessChar != key[2]) {
+     life--;
+     System.out.println("Try again:");
+     guessChar = input.next().charAt(0);
+   }
+   if (guessChar == key[3]) {
+     System.out.println("" + key[0] + "" + key[1] + "" + key[2] + "" + key[3]);
    } else if (guessChar != key[3]) {
-     lives--;
+     life--;
      System.out.println("Try again:");
+     guessChar = input.next().charAt(0);
    }
-   if (guessChar = key[4]) {
-     System.out.println("" + key[1] + "" + key[2] + "" + key[3] + "" + key[4]);
-   } else if (guessChar != key[4]) {
-     lives--;
-     System.out.println("Try again:");
-   }
-    if (lives = 0) {
+    if (lives = false) {
       System.out.println("You are out of lives - Game Over.");
     }
   }
